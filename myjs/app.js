f1bbf70e9cda17f080993c996bdc65c656023fab
SiThuKyaw <sithumyatnoe27@gmail.com>
2022-01-22 10:53:56 +0630

    first commit

---
+++ myjs/app.js
@@ -0,0 +1,8 @@
+// ********** Mobile Navbar **********
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function () {
+    linksContainer.classList.toggle('show-links');
+});
